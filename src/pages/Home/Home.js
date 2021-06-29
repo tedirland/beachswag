91c6a85b3c737a369de4832071ded5b379b24e93
Ted Irland <irlandth@gmail.com>
2021-06-29 14:25:57 -0500

    Created a spinner component and imported fontawesome to provide functionality

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
 import './Home.css'
 import SearchBox from './SearchBox'
+import Spinner from '../../utility/Spinner/Spinner'
+
 import axios from 'axios'
 
 export default class Home extends Component {
 
-  componentDidMount() {
-    const recommendedCities = axios.get()
+  state = {
+    cities: [],
+  }
+
+  async componentDidMount() {
+    const recommendedCities = await axios.get(`${window.apiHost}/cities/recommended`)
+    this.setState({cities: recommendedCities.data})
+
   }
   render() {
+    if(this.state.cities.length === 0) {
+      return (<Spinner />)
+    }
     return (
       <div className='container-fluid'>
         <div className='row'>
