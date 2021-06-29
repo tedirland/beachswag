c8eb79c35c4c7b21c124f353abb6bb7d52894f86
Ted Irland <irlandth@gmail.com>
2021-06-29 14:34:16 -0500

    got spinner working when page data is still loading

--- src/utility/Spinner/Spinner.js
+++ src/utility/Spinner/Spinner.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import './Spinner.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+library.add(faSpinner)
 
 export default class Spinner extends Component {
   render() {
     return (
-      <div>
-        Spinner
+      <div className='spinner-wrapper'>
+           <FontAwesomeIcon icon='spinner' size='5x' spin/>
       </div>
     )
   }
