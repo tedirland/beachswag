288b8f9bb6caba914aa5cbd858433f0e212ff9fe
Ted Irland <irlandth@gmail.com>
2021-06-30 05:59:27 -0500

    Created a cities component and exported the logic that maps through the cities returned in state to this component. Displayed to Homepage below the fold

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,36 +1,39 @@
-import React, { Component } from 'react'
-import './Home.css'
-import SearchBox from './SearchBox'
-import Spinner from '../../utility/Spinner/Spinner'
-
-import axios from 'axios'
+import React, { Component } from 'react';
+import './Home.css';
+import SearchBox from './SearchBox';
+import Spinner from '../../utility/Spinner/Spinner';
+import Cities from '../../utility/City/Cities';
+import axios from 'axios';
 
 export default class Home extends Component {
-
   state = {
     cities: [],
-  }
+  };
 
   async componentDidMount() {
-    const recommendedCities = await axios.get(`${window.apiHost}/cities/recommended`)
-    this.setState({cities: recommendedCities.data})
-
+    const recommendedCities = await axios.get(
+      `${window.apiHost}/cities/recommended`
+    );
+    this.setState({ cities: recommendedCities.data });
   }
   render() {
-    if(this.state.cities.length === 0) {
-      return (<Spinner />)
+    if (this.state.cities.length === 0) {
+      return <Spinner />;
     }
+
+    const recCities = <Cities cities={this.state.cities} />;
+    console.log(recCities);
     return (
-      <div className='container-fluid'>
-        <div className='row'>
-          <div className='home col s12'>
-            <div className='upper-fold'>
-            <SearchBox />
+      <div className="container-fluid">
+        <div className="row">
+          <div className="home col s12">
+            <div className="upper-fold">
+              <SearchBox />
             </div>
           </div>
+          {recCities}
         </div>
-
       </div>
-    )
+    );
   }
 }
--- src/utility/City/City.js
+++ src/utility/City/City.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react'
-import './City.css'
+import React, { Component } from 'react';
+import './City.css';
 
 export default class City extends Component {
   render() {
+    console.log(this.props.city);
+    const { cityName, image, price } = this.props.city;
     return (
-      <div>
-        City Component
+      <div className="city col s12">
+        <div className="image">
+          <img src={image} alt={cityName} />
+        </div>
+
+        <div className="city-name">{cityName}</div>
+        <div className="price">${price}/night average</div>
       </div>
-    )
+    );
   }
 }
