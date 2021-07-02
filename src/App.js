e44c013df42c91faa3a9ef65a288edf8e63614cf
Ted Irland <irlandth@gmail.com>
2021-07-02 10:39:21 -0500

    Created a modal and wrote code to open and close it depending on redux state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './utility/NavBar/Navbar';
+import Modal from './utility/Modal/Modal';
 import Home from './pages/Home/Home';
-import SingleFullVenue from './pages/SingleFullVenue/SingleFullVenue';
+import SingleFullVenue from '../src/pages/SingleFullVenue/SingleFullVenue';
 // import './App.css'
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
         <Route path="/" component={Navbar} />
         <Route exact path="/" component={Home} />
         <Route exact path="/venue/:vid" component={SingleFullVenue} />
+        <Route path="/" component={Modal} />
       </Router>
     );
   }
--- src/utility/NavBar/Navbar.js
+++ src/utility/NavBar/Navbar.js
@@ -1,34 +1,63 @@
-import React, { Component } from 'react'
-import './Navbar.css'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import openModal from '../../actions/openModal';
 
-export default class Navbar extends Component {
+class Navbar extends Component {
   render() {
-
     let navColor = 'transparent';
-    if(this.props.location.pathname !== '/'){
-      navColor = 'black'
+    if (this.props.location.pathname !== '/') {
+      navColor = 'black';
     }
     return (
-      <div className='container-fluid nav'>
-        <div className='row'>
+      <div className="container-fluid nav">
+        <div className="row">
           <nav className={navColor}>
-          <div className='nav-wrapper'>
-            <Link to='/' className='left'>Beachswag</Link>
-              <ul id='nav-mobile' className='right'>
-                <li><Link to='/'>English (US)</Link></li>
-                <li><Link to='/'>$ USD</Link></li>
-                <li><Link to='/'>Become a Host</Link></li>
-                <li><Link to='/'>Help</Link></li>
-                <li><Link to='/'>Sign Up</Link></li>
-                <li><Link to='/'>Log In</Link></li>
-            </ul>
-
-          </div>
+            <div className="nav-wrapper">
+              <Link to="/" className="left">
+                Beachswag
+              </Link>
+              <ul id="nav-mobile" className="right">
+                <li>
+                  <Link to="/">English (US)</Link>
+                </li>
+                <li>
+                  <Link to="/">$ USD</Link>
+                </li>
+                <li>
+                  <Link to="/">Become a Host</Link>
+                </li>
+                <li>
+                  <Link to="/">Help</Link>
+                </li>
+                <li>
+                  <Link to="/">Sign Up</Link>
+                </li>
+                <li
+                  onClick={() => {
+                    this.props.openModal('open', 'Log In');
+                  }}
+                >
+                  Log In
+                </li>
+              </ul>
+            </div>
           </nav>
         </div>
-
       </div>
-    )
+    );
   }
 }
+
+function mapDispatchToProps(dispatcher) {
+  return bindActionCreators(
+    {
+      openModal: openModal,
+    },
+    dispatcher
+  );
+}
+
+export default connect(null, mapDispatchToProps)(Navbar);
